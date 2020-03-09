a0130a0abedc4e5315770405ba0785e566fa2262
Marze1994 <zengjie9421@163.com>
2020-03-09 22:03:13 +0800

    Upgrade babel & Add perttier plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,31 @@
-// https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
   parserOptions: {
-    parser: 'babel-eslint',
-    sourceType: "module",
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      globalReturn: false,
+      impliedStrict: true,
+      jsx: true
+    },
+    requireConfigFile: false,
+    allowImportExportEverywhere: false
   },
   env: {
     browser: true,
-    es6: true
+    node: true,
+    amd: true,
+    commonjs: true,
+    es6: true,
+    jquery: true
   },
-  extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/base',
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    // 'standard'
-  ],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  // add your custom rules here
+  plugins: ['vue'],
+  extends: ['plugin:vue/base'],
   rules: {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
-}
+};
